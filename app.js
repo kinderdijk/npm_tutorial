da2c169a92913730188d9ab5319d247f3fc4662b
Jonathon Pendleury <jonathon.pendlebury@gmail.com>
2017-12-19 07:07:57 -0700

    Added a page for configuring the main start page. Still an error in getting the bootstrapcss into the routed pages.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var assert = require('assert');
+var mongoClient = require('mongodb').MongoClient;
 
 var app = express();
 
 var port = 2468;
-var postRouter = require('./src/routes/postRoutes');
-var adminRouter = require('./src/routes/adminRoutes');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -14,10 +14,29 @@ app.use(express.static('src/img'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+var postRouter = require('./src/routes/postRoutes');
 app.use('/Post', postRouter);
 
+var adminRouter = require('./src/routes/adminRoutes');
+app.use('/admin', adminRouter);
+
 app.get('/', function(req, res) {
-    res.render('intro.ejs');
+    var url = 'mongodb://localhost:27017/postLibrary';
+    
+    var latestPost = 'This is not the latest post.';
+    mongoClient.connect(url, function(err, database) {
+        assert.equal(null, err);
+        
+        var myDb = database.db('postLibrary');
+        var collection = myDb.collection('post');
+        
+        var mysort = { timestamp: -1 };
+        collection.find().sort(mysort).toArray(function(err, result) {
+            database.close()
+            latestPost = result[0].content;
+            res.render('intro.ejs', {post: latestPost});
+        });
+    });
 });
 
 app.listen(port, function(err) {
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,18 +2,23 @@ var express = require('express');
 var adminRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 
-var router = function() {
-    adminRouter.route('/addPost')
-        .get(function(req, res) {
-            var url = 'mongodb://localhost:27017/postLibrary';
-        
-            mongodb.connect(url, function(err, bd) {
-                var collection = bd.collection('posts');
-            });
-            res.render('managePost.ejs');
-        });
-    
-    return adminRouter;
-}
+adminRouter.route('/configureStartPage')
+    .get(function(req, res) {
+        var url = 'mongodb://localhost:27017/postLibrary';
 
-module.exports = router;
+        mongodb.connect(url, function(err, database) {
+            var myDb = database.db('postLibrary');
+            var postCollection = myDb.collection('post');
+            
+            var currentPosts = [];
+            postCollection.find().sort({timestamp: -1}).toArray(function(err, results) {
+                currentPosts = results;
+                
+                database.close();   
+                console.log('Results: ' + currentPosts);
+                res.render('configureStartPage', {currentPosts: currentPosts});
+            });
+        });
+    });
+
+module.exports = adminRouter;
