22fa33e3ff15fdab7774947e1725b476600d4595
Jonathon Pendleury <jonathon.pendlebury@gmail.com>
2017-12-23 20:09:00 -0700

    Added the ability to specify a post to edit. Added more UI functionality to the manage post page.

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,12 +1,64 @@
 var express = require('express');
 var postRouter = express.Router();
 var mongoClient = require('mongodb').MongoClient;
+var objectID = require('mongodb').ObjectID;
 var assert = require('assert');
+var util = require('util');
 
 postRouter.route('/edit').get(function(req, res) {
-    console.log('Loading post from separate routing file.');
-    res.render('editPost.ejs');
+    var postID = req.query.postID;
+    if(postID) {
+        var url = 'mongodb://localhost:27017/postLibrary';
+        
+        mongoClient.connect(url, function(err, database) {
+            assert.equal(null, err);
+
+            if (database.isConnected()) {
+                var myDb = database.db('postLibrary');
+                var postCollection = myDb.collection('post');
+
+                var currentPosts = [];
+                // The findOne function returns a promise. Not a standard object.
+                var result = postCollection.findOne({_id: new objectID(postID)});
+                result.then(function(postValues) {
+                    res.render('editPost.ejs', {post: postValues});
+                });
+                database.close();
+                
+            } else {
+                console.log('Database is not connected.');
+            }
+        });
+    } else {
+        res.render('editPost.ejs', {post: ''});
+    }
+    
 });
+
+postRouter.route('/manage').get(function(req, res) {
+    var url = 'mongodb://localhost:27017/postLibrary';
+        
+    mongoClient.connect(url, function(err, database) {
+        assert.equal(null, err);
+        console.log('Connection to the database correctly: ' + database);
+        
+        if (database.isConnected()) {
+            console.log('Database is connected. Attempting function calls.');
+            var myDb = database.db('postLibrary');
+            var postCollection = myDb.collection('post');
+            
+            var currentPosts = [];
+            postCollection.find().sort({timestamp: -1}).toArray(function(err, results) {
+                currentPosts = results;
+                
+                database.close();
+                res.render('managePost.ejs', {posts: currentPosts});
+            });
+        } else {
+            console.log('Database is not connected.');
+        }
+    });
+})
 
 postRouter.post('/write', function(req, res) {
     let title = req.body['title'];
